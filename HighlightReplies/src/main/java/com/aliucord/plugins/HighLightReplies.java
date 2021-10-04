e3e688168f388792834ca9105d645efffaeeaed3
Manti <67705577+mantikafasi@users.noreply.github.com>
2021-10-04 23:16:07 +0300

    uhv3

---
+++ HighlightReplies/src/main/java/com/aliucord/plugins/HighLightReplies.java
@@ -0,0 +1,29 @@
+package com.aliucord.plugins;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.aliucord.annotations.AliucordPlugin;
+import com.aliucord.api.CommandsAPI;
+import com.aliucord.entities.Plugin;
+
+import java.util.Collections;
+
+@SuppressWarnings("unused")
+@AliucordPlugin
+public class HighLightReplies extends Plugin {
+
+
+
+    @Override
+    public void start(Context context) {
+    
+    }
+
+    @Override
+    public void stop(Context context) {
+        patcher.unpatchAll();
+        commands.unregisterAll();
+    }
+}
