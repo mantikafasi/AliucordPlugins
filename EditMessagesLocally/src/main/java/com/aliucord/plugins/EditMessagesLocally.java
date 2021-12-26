c455f2c02889f0cab8fae6b1a93fdb6b629be420
Manti <67705577+mantikafasi@users.noreply.github.com>
2021-12-26 19:17:05 +0300

    editmessageslocally

---
+++ EditMessagesLocally/src/main/java/com/aliucord/plugins/EditMessagesLocally.java
@@ -0,0 +1,90 @@
+package com.aliucord.plugins;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+import androidx.core.widget.NestedScrollView;
+
+import com.aliucord.CollectionUtils;
+import com.aliucord.Utils;
+import com.aliucord.annotations.AliucordPlugin;
+import com.aliucord.entities.Plugin;
+import com.aliucord.patcher.Hook;
+import com.aliucord.utils.ReflectUtils;
+import com.discord.models.user.CoreUser;
+import com.discord.stores.StoreStream;
+import com.discord.utilities.messagesend.MessageQueue;
+import com.discord.utilities.messagesend.MessageRequest;
+import com.discord.widgets.chat.list.WidgetChatList;
+import com.discord.widgets.chat.list.actions.WidgetChatListActions;
+import com.discord.widgets.chat.list.entries.MessageEntry;
+
+@SuppressWarnings("unused")
+@AliucordPlugin
+public class EditMessagesLocally extends Plugin {
+    int viewid = View.generateViewId();
+    long me = StoreStream.getUsers().getMe().getId();
+
+    @Override
+    public void start(Context context) throws NoSuchMethodException {
+        patcher.patch(MessageQueue.class.getDeclaredMethod("doEdit", MessageRequest.Edit.class, MessageQueue.DrainListener.class), new Hook((cf) -> {
+            var edit = (MessageRequest.Edit) cf.args[0];
+            var channelId = edit.getChannelId();
+            var messageId = edit.getMessageId();
+            var content = edit.getContent();
+            var mes = StoreStream.getMessages().getMessage(channelId, messageId);
+            if (!(new CoreUser(mes.getAuthor()).getId() == me)) {
+                try {
+                    ReflectUtils.setField(mes, "content", content);
+                } catch (NoSuchFieldException | IllegalAccessException e) {
+                    logger.error(e);
+                }
+                StoreStream.access$handleMessageUpdate(StoreStream.getVoiceParticipants().getStream(), mes.synthesizeApiMessage());
+
+                Utils.mainThread.post(() -> {
+                    var wchlist = WidgetChatList.access$getAdapter$p(Utils.widgetChatList);
+                    int i = CollectionUtils.findIndex(wchlist.getInternalData(), e -> e instanceof MessageEntry && ((MessageEntry) e).getMessage().getId() == mes.getId());
+                    wchlist.notifyItemChanged(i);
+                });
+                cf.setResult(null);
+            }
+        }));
+
+
+        Drawable editIcon = ContextCompat.getDrawable(context, com.lytefast.flexinput.R.e.ic_edit_24dp).mutate();
+
+        patcher.patch(WidgetChatListActions.class.getDeclaredMethod("configureUI", WidgetChatListActions.Model.class),
+                new Hook((cf) -> {
+                    var modal = (WidgetChatListActions.Model) cf.args[0];
+                    var message = modal.getMessage();
+                    var actions = (WidgetChatListActions) cf.thisObject;
+                    var scrollView = (NestedScrollView) actions.getView();
+                    var lay = (LinearLayout) scrollView.getChildAt(0);
+                    if (lay.findViewById(viewid) == null && message.getAuthor().i() != StoreStream.getUsers().getMe().getId()) {
+                        TextView tw = new TextView(lay.getContext(), null, 0, com.lytefast.flexinput.R.i.UiKit_Settings_Item_Icon);
+                        tw.setId(viewid);
+                        tw.setText("Edit Message Locally");
+                        tw.setCompoundDrawablesRelativeWithIntrinsicBounds(editIcon, null, null, null);
+                        lay.addView(tw, 5);
+                        tw.setOnClickListener((v) -> {
+                            try {
+                                ReflectUtils.invokeMethod(cf.thisObject, "editMessage", message);
+                            } catch (ReflectiveOperationException e) {
+                                logger.error(e);
+                            }
+                            actions.dismiss();
+                        });
+                    }
+                }));
+    }
+
+    @Override
+    public void stop(Context context) {
+        patcher.unpatchAll();
+        commands.unregisterAll();
+    }
+}
