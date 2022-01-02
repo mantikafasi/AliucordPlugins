46c89f1d1cdd1e4120218a9a3ac12f305dbedc94
Manti <67705577+mantikafasi@users.noreply.github.com>
2022-01-02 12:06:46 +0300

    made pluginwiper

---
+++ PluginWiper/src/main/java/com/aliucord/plugins/PluginWiper.java
@@ -0,0 +1,37 @@
+package com.aliucord.plugins;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.aliucord.Constants;
+import com.aliucord.PluginManager;
+import com.aliucord.Utils;
+import com.aliucord.annotations.AliucordPlugin;
+import com.aliucord.api.CommandsAPI;
+import com.aliucord.entities.Plugin;
+
+import java.io.File;
+import java.util.Collections;
+
+@SuppressWarnings("unused")
+@AliucordPlugin
+public class PluginWiper extends Plugin {
+
+    @Override
+    public void start(Context context) {
+        logger.info(PluginManager.plugins.toString());
+        for(var plugin:PluginManager.plugins.values()) {
+            logger.info("Deleting " + plugin.__filename);
+            logger.info(new File(Constants.PLUGINS_PATH, plugin.__filename +".zip").delete() ? " Deleted" + plugin.__filename : " Couldnt delete");
+        }
+        Utils.showToast("Deleted All Plugins");
+        Utils.showToast("Dont install unreleased plugins dumbass");
+    }
+
+    @Override
+    public void stop(Context context) {
+        patcher.unpatchAll();
+        commands.unregisterAll();
+    }
+}
